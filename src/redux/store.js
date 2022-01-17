44313b154ef4d62e46c6865da6e232d6bd418a86
Agata Jarmoszuk <agnes365@vp.pl>
2022-01-17 22:23:08 +0100

    Add search

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,6 +8,8 @@ const reducer = (state, action) => {
             return { ...state, columns: [...state.columns, { id: shortid(), ...action.payload }] };
         case 'ADD_CARD':
             return { ...state, cards: [...state.cards, { id: shortid(), ...action.payload }] };
+        case 'UPDATE_SEARCHSTRING':
+            return { ...state, searchString: action.payload };
         default:
             return state;
     }
@@ -15,7 +17,7 @@ const reducer = (state, action) => {
 
 const store = createStore(
     reducer,
-    initialState,
+    { ...initialState, searchString: '' },
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
