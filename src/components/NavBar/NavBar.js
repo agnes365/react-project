46656b340e8c60fcfe1be20ce15a514d351e459d
Agata Jarmoszuk <agnes365@vp.pl>
2022-01-18 00:36:19 +0100

    Add navigation

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,21 @@
+import styles from './NavBar.module.scss';
+import Container from '../Container/Container'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <Container>
+                <div className={styles.elements}>
+                    <a href="/"><span className={styles.icon + ' fa fa-tasks'} /></a>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/favorite”">Favorite</a></li>
+                        <li><a href="/about">About</a></li>
+                    </ul>
+                </div>
+            </Container>
+        </nav>
+    )
+};
+
+export default NavBar;
