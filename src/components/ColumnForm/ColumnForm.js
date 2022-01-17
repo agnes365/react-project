b0fe832c5a9fd0a176a1e478383b348249d88d36
Agata Jarmoszuk <agnes365@vp.pl>
2022-01-17 21:40:41 +0100

    Add data modification by redux

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -2,21 +2,24 @@ import styles from './ColumnForm.module.scss';
 import Button from './../Button/Button';
 import TextInput from './../TextInput/TextInput';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
-const ColumnForm = props => {
+const ColumnForm = () => {
     const [title, setTitle] = useState('');
     const [icon, setIcon] = useState('');
 
+    const dispatch = useDispatch();
+
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: title, icon: icon });
+        dispatch({ type: 'ADD_COLUMN', payload: { title, icon } });
         setTitle('');
         setIcon('');
     };
 
     return (
         <form className={styles.columnForm} onSubmit={handleSubmit}>
-            <span>Title:</span><TextInput value={title} onChange={e => setTitle(e.target.value)} /> 
+            <span>Title:</span><TextInput value={title} onChange={e => setTitle(e.target.value)} />
             <span>Icon:</span><TextInput value={icon} onChange={e => setIcon(e.target.value)} />
             <Button>Add column</Button>
         </form>
