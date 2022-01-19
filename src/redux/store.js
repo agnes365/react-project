1c64aa273c32d41861f015aa3f46d41652e65439
Agata Jarmoszuk <agnes365@vp.pl>
2022-01-19 23:16:16 +0100

    Finish 14.3

--- src/redux/store.js
+++ src/redux/store.js
@@ -18,6 +18,8 @@ export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 
 export const addCard = payload => ({ type: 'ADD_CARD', payload });
 
+export const addList = payload => ({ type: 'ADD_LIST', payload });
+
 export const updateSearchString = (search) => ({ type: 'UPDATE_SEARCHSTRING', payload: search });
 
 const reducer = (state, action) => {
@@ -28,6 +30,8 @@ const reducer = (state, action) => {
             return { ...state, cards: [...state.cards, { id: shortid(), ...action.payload }] };
         case 'UPDATE_SEARCHSTRING':
             return { ...state, searchString: action.payload };
+        case 'ADD_LIST':
+            return { ...state, lists: [...state.lists, { id: shortid(), ...action.payload }] };
         default:
             return state;
     }
